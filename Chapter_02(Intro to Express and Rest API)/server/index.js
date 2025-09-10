908b3e909becfe2f5ec1bfe3f9f868422b63103b
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2025-09-11 01:25:21 +0530

    Learned about Route Parameters (Params)

--- Chapter_02(Intro to Express and Rest API)/server/index.js
+++ Chapter_02(Intro to Express and Rest API)/server/index.js
@@ -10,23 +10,100 @@ const app = express()
 
 // Every route can be created Independently ! 
 
-app.get('/' , (req , res) => {
-  res.send("Hello from Home Page")
-})
+// app.get('/' , (req , res) => {
+//   res.send("Hello from Home Page")
+// })
 
-app.get('/about' , (req , res) => {
-  res.send("Hello from About Page")
-})
+// app.get('/about' , (req , res) => {
+//   res.send("Hello from About Page")
+// })
 
-app.get('/contact' , (req , res) => {
-  res.send("Hello from Contact Page")
-})
-
-app.listen(8001, () => {
-  console.log('Server Running!')
-})
+// app.get('/contact' , (req , res) => {
+//   res.send("Hello from Contact Page")
+// })
 
 
 
 // This is more clean and Easy with Express!
   
+
+// What is this Object ? 
+
+// One perticular route can make Requests to multiple Resources ! 
+
+// Url keep changing ! Adding more and more as we click more ! 
+
+// Lets make it ! >> Routes Parameters ! 
+
+// how to set them up ? 
+
+// take helo of Properties ! 
+
+
+
+
+
+// Making a Dummy List //
+
+let courses = [
+  {id: 1 , name : "Jave"} , 
+  {id: 2 , name : 
+    "Dev"
+  },
+  {id : 3, name :
+    "DBMS"
+  }
+]
+
+
+
+
+
+
+// Made a courses rout 
+
+// Get all courses ! 
+app.get('/courses' , (req , res) => {
+  res.send(courses)
+})
+
+
+
+// this will show all the courses ! 
+// getting this is JSON !
+
+// lets make changes with more links ! 
+
+// setting up route Parameters ! 
+
+
+
+
+
+
+// get courses with id 
+app.get('/courses:id' , (req , res) => {
+
+
+  // console.log(req.params) // THIS WILL GET OBJECT IN STRING ! 
+
+
+   // this is special object ! 
+  // this route will store in req.params ! 
+
+  // Id -> Key 
+  // Value -> Value 
+
+  // Specific Id > Specif Course 
+
+  
+  let course = courses.find((course) => course.id === parseInt(req.params.id));  // find method gives us the first match ! 
+
+
+
+})
+
+
+app.listen(8001, () => {
+  console.log('Server Running!')
+})
