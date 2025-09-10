09d3334b9c50c053541e7f13ae5a7bda112360a6
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2025-09-10 17:55:31 +0530

    This is Chapter 1 and i learned to Handle File Operations(CRUD)

---
+++ Chapter_01 (Intro to Node Js) /FileSystem/fs.js
@@ -0,0 +1,32 @@
+// CURD > READ > CREATE > UPDATE > DETETE 
+
+// require it ? Import it 
+
+const fs = require('fs') // > FS MODULE IMPORTED ! 
+
+
+
+// Start 
+console.log("Start")
+
+// Read a file ! (SYNC)
+
+let data = fs.readFileSync('f1.txt') 
+// console.log(data)// we get a buffer > This is System generated encoding ! 
+console.log("Data from file 1 is  > " + data)
+
+
+// READING FILE ASYNC 
+
+fs.readFile('f2.txt' , (err , data) => { // THis pattern is called >>>> Error first >>>> Called >> Error First callback !! 
+  if(err){
+    console.error(err)
+  }
+  console.log("Data from file 2 is > " + data) 
+}) // THIS TAKES A CALLBACK ! 
+
+
+console.log("Start") 
+
+
+// Start > File 1 > END > File 2 
